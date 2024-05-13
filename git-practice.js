0bb522ea6d425d454cc5a77cf8a4f3e3134caed2
Charles B. <charlesbanks238@gmail.com>
2024-05-13 09:05:30 -0700

    Adds a JS file.

---
+++ git-practice.js
@@ -0,0 +1,9 @@
+function gitPractice (didItWork) {
+    if (didItWork === 'yes') {
+        console.log('Yay! It worked!')
+    } else if (didItWork === 'No') {
+        console.log('How are you here??')
+    }
+}
+
+gitPractice (?)
